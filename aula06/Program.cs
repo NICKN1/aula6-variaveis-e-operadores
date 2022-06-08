3314db3b1be9398ce23a3f457c7c6ddbe88a00cd
Fabiana Sayuri <fabianakamamoto@hotmail.com>
2022-06-08 16:48:11 -0300

    mesa de trabalho

---
+++ aula06/Program.cs
@@ -0,0 +1,21 @@
+﻿//Variáveis
+string nome;
+string idade;
+string anoAtual;
+
+
+
+Console.WriteLine("Digite o seu nome: ");
+nome = Console.ReadLine();
+
+Console.WriteLine("Digite a sua idade: ");
+idade = Console.ReadLine();
+
+Console.WriteLine("Digite o ano atual: ");
+anoAtual = Console.ReadLine();
+
+//Calculo com operador aritmético
+int anoNasceu = Int32.Parse(anoAtual) - Int32.Parse(idade);
+
+//Output com os dados!
+Console.WriteLine($"Olá {nome}, você nasceu em {anoNasceu}");
